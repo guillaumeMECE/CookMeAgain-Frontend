2d0cdb4421e29299060a32fc6e11e5658c57a1aa
Guillaume <guillaume.maurin@edu.ece.fr>
2020-04-15 12:00:50 +0200

    Sign in with Google

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 import { withRouter } from 'react-router-dom';
 import Routes from './routes';
 
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './helpers/firebaseConfig';
+
 import Container from "react-bootstrap/Container"
 
 import AppBar from './components/appBar';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
 
 const renderPage = () => {
   return (<Routes />);
@@ -13,9 +25,26 @@ const renderPage = () => {
 
 class App extends React.Component {
   render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+    console.log("USER : ",user);
+    
     return (
       <div className="App">
         <AppBar />
+        {
+          user
+            ? <p>Hello, {user.displayName}</p>
+            : <p>Please sign in.</p>
+        }
+        {
+          user
+            ? <button onClick={signOut}>Sign out</button>
+            : <button onClick={signInWithGoogle}>Sign in with Google</button>
+        }
         <Container className="p-0">
           {renderPage()}
         </Container>
@@ -24,4 +53,9 @@ class App extends React.Component {
   };
 }
 
-export default withRouter(props => <App {...props} />);
+// export default withRouter(props => <App {...props} />);
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(withRouter(props => <App {...props} />));
---
+++ src/helpers/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCDiPJf4jBKaOiabbFBZopQXs9X24bKpFg",
+    authDomain: "cook-me-again.firebaseapp.com",
+    databaseURL: "https://cook-me-again.firebaseio.com",
+    projectId: "cook-me-again",
+    storageBucket: "cook-me-again.appspot.com",
+    messagingSenderId: "128319791734",
+    appId: "1:128319791734:web:65a3b4c1627d33a5537557",
+    measurementId: "G-VKPYP39EE8"
+  };
+
+  export default firebaseConfig;
