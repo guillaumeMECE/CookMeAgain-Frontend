e14a6aaffa74fb4482d471ab10d81610a1bf1dcf
Guillaume <guillaume.maurin@edu.ece.fr>
2020-04-16 17:08:48 +0200

    Add Goback button

--- src/components/appBar/index.js
+++ src/components/appBar/index.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react'
 import "./style.css"
 
+import {Link} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
+
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCookieBite } from '@fortawesome/free-solid-svg-icons'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
-export default class AppBar extends Component {
+ class AppBar extends Component {
     constructor(props) {
         super(props);
         this.state = {};
+    }
+    goBack(){
+        console.log(this.props.history);
+         this.props.history.goBack()
     }
 
     render() {
         return (
             <div className="AppBar sticky-top ">
                 <Navbar className="shadow-lg" bg="primary" variant="dark">
-
+                    {this.props.history.location.pathname==="/" ?
+                    "":
+                    <Navbar.Brand id="brand" onClick={()=>{this.goBack()}}>
+                    {/* <Link to="/"> */}
+                        <FontAwesomeIcon icon={faChevronLeft} size="lg" color="white" />
+                    {/* </Link> */}
+                    </Navbar.Brand>
+                    }
+               
                     <Navbar.Brand id="brand">
                         <FontAwesomeIcon icon={faCookieBite} size="lg" color="white" />
                         {' '}
@@ -36,3 +52,5 @@ export default class AppBar extends Component {
         );
     }
 }
+
+export default withRouter(AppBar)
